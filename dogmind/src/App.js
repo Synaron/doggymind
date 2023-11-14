5c4c7fe7120e2457957469e6dfb1d00790989af3
kran12ak ak <kran12ak@gmail.com>
2023-11-14 12:09:42 +0000

    Create slider component

--- dogmind/src/App.js
+++ dogmind/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Header from "./components/Header";
+import Slider from "./components/Slider";
 
 
 function App() {
   return (
     <div className="App">
-        <Header title="DOGMIND" />
+        <Slider />
     </div>
   );
 }
