cf48ba29612eb9bda43f035e39e34833727a4911
kran12ak ak <kran12ak@gmail.com>
2023-11-24 14:54:56 +0000

    Footer erstellen

--- dogmind/src/components/Footer.js
+++ dogmind/src/components/Footer.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        // Footer.js
+import React from 'react';
+import Column from './Column'; // Create a separate Column component
+
+const Footer = () => {
+  return (
+    <footer>
+        <Column title="Data and Impressum">
+            <p>Impressum</p>
+            <p>Datenschutz</p>
+        </Column>
+
+        <Column title="Copyright">
+            <p>&copy; 2023 Your Company Name. All Rights Reserved.</p>
+        </Column>
+
+        <Column title="Social Media">
+            {/* Add social media icons and links here */}
+        </Column>
+
+      
+    </footer>
+  );
+};
+
+export default Footer;
+
+    </div>
   )
 }
 
