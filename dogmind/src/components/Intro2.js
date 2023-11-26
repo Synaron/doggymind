31e190f0cd0eb6e6afb63ec2dc1b312fa8d3fa40
kran12ak ak <kran12ak@gmail.com>
2023-11-26 18:11:07 +0000

    Debug intro css

--- dogmind/src/components/Intro2.js
+++ dogmind/src/components/Intro2.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ImageBlock from './ImageBlock';
+import '../assets/css/Intro2.css';
 
 function Intro2({
   firstWord,
@@ -27,7 +28,7 @@ function Intro2({
   return (
     <Container fluid id="Intro2">
       <Row style={{ display: 'flex', alignItems: 'stretch' }}>
-        <Col className='intro-text-block' md={6} >
+        <Col className='intro-text' md={6} >
           <h1>
             {firstWord} <span style={{ color: '#F6BE00' }}>{secondWord}</span>{' '}
             {secondPhrase}
