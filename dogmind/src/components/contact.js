e660cc67ab8feb6f6899add99fce8cf4e0b5c7e1
kran12ak ak <kran12ak@gmail.com>
2024-08-22 19:26:39 +0000

    Anpassen des Formulars

--- dogmind/src/components/contact.js
+++ dogmind/src/components/contact.js
@@ -8,8 +8,8 @@ export const ContactUs = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-        publicKey: 'YOUR_PUBLIC_KEY',
+      .sendForm('service_ce53x1b', 'template_rck0n0n', form.current, {
+        publicKey: 'JgMmuVjh0G2U1PPwC',
       })
       .then(
         () => {
@@ -25,10 +25,18 @@ export const ContactUs = () => {
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
       <input type="text" name="user_name" />
+      <label>Anschrift</label>
+      <input type="text" name="anschrift" />
+      <label>Telefonnummer:</label>
+      <input type="text" name="telefonnummer" />
       <label>Email</label>
       <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
+      <label>Alter des Hundes:</label>
+      <input type="text" name="alter_des_hundes" />
+      <label>Hunderasse:</label>
+      <input type="text" name="rasse" />
+      <label>Beschreibung des Anliegens</label>
+      <textarea name="beschreibung" />
       <input type="submit" value="Send" />
     </form>
   );
